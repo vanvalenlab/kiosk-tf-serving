3a3ad73a807472f6360ea4759f038432feb800a3
Dylan Bannon <dylanbannon5@gmail.com>
2018-09-28 09:56:26 -0700

    all necessary files

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ARG TF_SERVING_VERSION=1.10.1
+ARG TF_SERVING_BUILD_IMAGE=tensorflow/serving:${TF_SERVING_VERSION}-devel-gpu
+
+FROM ${TF_SERVING_BUILD_IMAGE} as build_image
+FROM nvcr.io/vvlab/tensorflow:18.04-py3
+#FROM nvidia/cuda:9.0-cudnn7-runtime-ubuntu16.04
+
+ARG TF_SERVING_VERSION_GIT_BRANCH=master
+ARG TF_SERVING_VERSION_GIT_COMMIT=head
+
+LABEL maintainer="gvasudevan@google.com"
+LABEL tensorflow_serving_github_branchtag=${TF_SERVING_VERSION_GIT_BRANCH}
+LABEL tensorflow_serving_github_commit=${TF_SERVING_VERSION_GIT_COMMIT}
+
+# Dealing with a keyboard issue
+COPY ./keyboard /etc/default/keyboard
+
+RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y --no-install-recommends \
+        libgomp1 \
+        ca-certificates \
+        && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+        #nvidia-cuda-toolkit \
+
+# Install TF Serving GPU pkg
+COPY --from=build_image /usr/local/bin/tensorflow_model_server /usr/bin/tensorflow_model_server
+
+# Including our functions
+COPY generate_config.py /opt/generate_config.py
+COPY models.conf /opt
+COPY tf_serving_startup.sh /
+RUN chmod 755 /tf_serving_startup.sh
+CMD ["/bin/sh", "-c", "/tf_serving_startup.sh"]
+
+
+#CMD ["/bin/bash"]
---
+++ generate_config.py
@@ -0,0 +1,86 @@
+"""
+generate_config.py
+@author: andrewqho, willgraf
+
+Script to generate Tensorflow Serving configuration file at Docker Build Time
+
+The generate_config script pulls all subdirectories from a given bucket
+and model home directory and generates the required configuration file
+for Tensorflow Serving
+"""
+
+import os
+import boto3
+import argparse
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# Generate model.conf
+def gen_config(aws_s3_bucket, aws_access_key_id, aws_secret_access_key, model_prefix): 
+    # Create boto client object
+    s3client = boto3.client('s3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key)
+    
+    # Get every object in specified bucket
+    directories_verbose = s3client.list_objects_v2(Bucket=aws_s3_bucket, StartAfter=model_prefix)
+    
+    # Get unique list of model names
+    models_list = set([])
+    
+    for directory in directories_verbose['Contents']:
+        directory_name = directory['Key'].replace(model_prefix, '').split('/')
+        models_list.add(directory_name[0])
+    
+    # Create config file and write config block for each model
+    conf_file_path = os.path.join(ROOT_DIR, 'models.conf')
+    with open(conf_file_path, 'w+') as config_file:
+    
+        config_file.write('model_config_list: {\n')
+    
+        for model in models_list:
+            config_file.write('    config: {\n')
+            config_file.write('        name: "{}"\n'.format(model))
+            config_file.write('        base_path: "s3://{}/{}{}"\n'.format(aws_s3_bucket, model_prefix, model))
+            config_file.write('        model_platform: "tensorflow"\n')
+            config_file.write('        model_version_policy: {\n')
+            config_file.write('            all: {}\n')
+            config_file.write('        }\n')
+            config_file.write('    }\n')
+        config_file.write('}\n')
+
+def get_arg_parser():
+    """argument parser to consume command line arguments"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model_prefix',
+                        help='Base directory of models')
+
+    parser.add_argument('--aws_s3_bucket',
+                        help='specify target S3 Bucket')
+
+    parser.add_argument('--aws_access_key_id', 
+                        help='Access Key ID for S3 credentials')
+
+    parser.add_argument('--aws_secret_access_key',
+                        help='Secret Access Key for s3 credentials')
+
+    return parser
+
+if __name__ == '__main__':
+    # Get command line arguments
+    ARGS = get_arg_parser().parse_args()    
+
+    AWS_S3_BUCKET = str(ARGS.aws_s3_bucket)
+    AWS_ACCESS_KEY_ID = str(ARGS.aws_access_key_id)
+    AWS_SECRET_ACCESS_KEY = str(ARGS.aws_secret_access_key)
+    MODEL_PREFIX = str(ARGS.model_prefix)
+
+    if not MODEL_PREFIX.endswith('/'):
+        MODEL_PREFIX = MODEL_PREFIX + '/'
+    
+    # Generate configuration file
+    gen_config(AWS_S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, MODEL_PREFIX)
+
