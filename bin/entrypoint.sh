6c03d56b08ebcf7317d0ef5855e2fced00811c94
William Graf <7930703+willgraf@users.noreply.github.com>
2019-03-20 14:50:54 -0700

    update default env var names for tf serving consistency

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -12,9 +12,9 @@ python write_config_file.py \
     --file-path=$MODEL_CONFIG_FILE \
   && \
 tensorflow_model_server \
-    --port=$RPC_PORT \
-    --rest_api_port=$REST_PORT \
-    --rest_api_timeout_in_ms=$REST_TIMEOUT \
+    --port=$PORT \
+    --rest_api_port=$REST_API_PORT \
+    --rest_api_timeout_in_ms=$REST_API_TIMEOUT \
     --model_config_file=$MODEL_CONFIG_FILE \
     --enable_batching=$ENABLE_BATCHING \
     --grpc_channel_arguments=$GRPC_CHANNEL_ARGS \
