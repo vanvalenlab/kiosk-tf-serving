264312a73bd18c8c0c7b42b717750280441392a5
William Graf <7930703+willgraf@users.noreply.github.com>
2019-05-30 15:40:51 -0700

    create batching and monitoring config files on the fly.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# write the configuration file
+# write the configuration files, then run the server
 python write_config_file.py \
     --cloud-provider=$CLOUD_PROVIDER \
     --model-prefix=$MODEL_PREFIX \
     --file-path=$MODEL_CONFIG_FILE \
+  && \
+echo "prometheus_config: {" > $BATCHING_CONFIG_FILE
+echo "  enable: ${PROMETHEUS_MONITORING_ENABLED}," >> $BATCHING_CONFIG_FILE
+echo "  path: ${PROMETHEUS_MONITORING_PATH}" >> $BATCHING_CONFIG_FILE
+echo "}" >> $MONITORING_CONFIG_FILE
+  && \
+echo "max_batch_size { value: ${MAX_BATCH_SIZE} }" > $MONITORING_CONFIG_FILE
+echo "batch_timeout_micros { value: ${BATCH_TIMEOUT_MICROS} }" >> $MONITORING_CONFIG_FILE
+echo "max_enqueued_batches { value: ${MAX_ENQUEUED_BATCHES} }" >> $MONITORING_CONFIG_FILE
+echo "num_batch_threads { value: $(nproc) }" >> $MONITORING_CONFIG_FILE
   && \
 tensorflow_model_server \
     --port=$PORT \
@@ -14,6 +24,7 @@ tensorflow_model_server \
     --enable_batching=$ENABLE_BATCHING \
     --grpc_channel_arguments=$GRPC_CHANNEL_ARGS \
     --tensorflow_session_parallelism=$TF_SESSION_PARALLELISM \
-    --monitoring_config_file=./monitoring_config.txt \
+    --monitoring_config_file=$MONITORING_CONFIG_FILE \
+    --batching_parameters_file=$BATCHING_CONFIG_FILE \
   && \
 /bin/bash # hack to keep from exiting
