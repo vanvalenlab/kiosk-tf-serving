a5bfb6800803950330a2f53fba7f9851aaeac49b
willgraf <7930703+willgraf@users.noreply.github.com>
2020-10-06 10:36:08 -0700

    Use separate containers for tensorflow-serving from the config writers (#35)
    * Use `requirements-test.txt` pattern and pin `pytest<6` for `pytest-pep8` compatibility.
    
    * Split `Dockerfile` and `entrypoint.sh` into 2 files for the writer and server.
    
    * Deploy both Docker images on release.
    
    * Build model server options carefully to prevent conflicts.

---
+++ bin/write.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# write the configuration files before running the server
+python write_config_file.py \
+    --cloud-provider=$CLOUD_PROVIDER \
+    --model-prefix=$MODEL_PREFIX \
+    --file-path=$MODEL_CONFIG_FILE \
+    --monitoring-enabled=$PROMETHEUS_MONITORING_ENABLED \
+    --monitoring-path=$PROMETHEUS_MONITORING_PATH \
+    --monitoring-file-path=$MONITORING_CONFIG_FILE \
+    --enable-batching=$MONITORING_CONFIG_FILE \
+    --max-batch-size=$MAX_BATCH_SIZE \
+    --batch-timeout=$BATCH_TIMEOUT_MICROS \
+    --max-enqueued-batches=$MAX_ENQUEUED_BATCHES \
+    --batch-file-path=$BATCHING_CONFIG_FILE
