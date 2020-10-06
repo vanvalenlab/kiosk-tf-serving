0a3b5c4c47d796be2d51beae975f9b600d22a0e7
William Graf <7930703+willgraf@users.noreply.github.com>
2020-10-06 10:25:45 -0700

    Build model server options carefully to prevent conflicts.
    Fixes #26.

--- bin/serve.sh
+++ bin/serve.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-tensorflow_model_server \
-    --port=$PORT \
-    --rest_api_port=$REST_API_PORT \
-    --rest_api_timeout_in_ms=$REST_API_TIMEOUT \
-    --model_config_file=$MODEL_CONFIG_FILE \
-    --enable_batching=$ENABLE_BATCHING \
-    --grpc_channel_arguments=$GRPC_CHANNEL_ARGS \
-    --tensorflow_session_parallelism=$TF_SESSION_PARALLELISM \
-    --monitoring_config_file=$MONITORING_CONFIG_FILE \
-    --batching_parameters_file=$BATCHING_CONFIG_FILE \
-  && \
-/bin/bash # hack to keep from exiting
+# Add options for tensorflow_model_server based on settings
+export options=(
+  "--port=$PORT"
+  "--rest_api_port=$REST_API_PORT"
+  "--rest_api_timeout_in_ms=$REST_API_TIMEOUT"
+  "--enable_batching=$ENABLE_BATCHING"
+  "--grpc_channel_arguments=$GRPC_CHANNEL_ARGS"
+  "--tensorflow_session_parallelism=$TF_SESSION_PARALLELISM"
+  "--model_config_file=$MODEL_CONFIG_FILE"
+)
+
+# If PROMETHEUS_MONITORING_ENABLED, provide the monitoring config file.
+if [ "${PROMETHEUS_MONITORING_ENABLED}" == "true" ] ; then
+  echo "Using monitoring config file: $MONITORING_CONFIG_FILE"
+  options+=("--monitoring_config_file=$MONITORING_CONFIG_FILE") ;
+fi
+
+# If ENABLE_BATCHING, provide the batching config file.
+if [ "${ENABLE_BATCHING}" == "true" ] ; then
+  echo "Using batching config file: $BATCHING_CONFIG_FILE"
+  options+=("--batching_parameters_file=$BATCHING_CONFIG_FILE") ;
+fi
+
+tensorflow_model_server "${options[@]}" && /bin/bash # don't exit
